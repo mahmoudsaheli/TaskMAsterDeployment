1667a844e6ff62702073fcd4fe9eb5356fca9dc3
mahmoudsaheli <mahmoud_alsahily@hotmail.com>
2025-08-04 16:45:49 -0400

    update2

--- app.js
+++ app.js
@@ -77,10 +77,7 @@ app.use(function(err, req, res, next) {
 module.exports = app
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`server running at http://localhost:${PORT}`);
-});
+
 
 
 hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
